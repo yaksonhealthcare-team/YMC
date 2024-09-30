a7eab3565e12c30c6da699cab40c34d26ff8bd35
marco <yeajihyun@naver.com>
2024-09-30 11:33:48 +0900

    chore: component name change(Reload > ReloadIcon)

--- src/components/icons/ReloadIcon.tsx
+++ src/components/icons/ReloadIcon.tsx
@@ -12,7 +12,7 @@ export default function BellIcon(props: SvgIconProps) {
       >
         <path
           d="M12.7527 9.66663C12.0741 11.7959 10.1304 13.3333 7.83855 13.3333C4.98229 13.3333 2.66683 10.9455 2.66683 7.99996C2.66683 5.05444 4.98229 2.66663 7.83855 2.66663C9.75281 2.66663 11.4242 3.73916 12.3184 5.33329M10.7476 5.99996H13.3335V3.33329"
-          stroke="white"
+          stroke="black"
           strokeLinecap="round"
           strokeLinejoin="round"
         />
