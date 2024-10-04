3ce4f4fb58aca2adca612d855e5a848f8a48a658
Jinha <ceo@onboarders.co.kr>
2024-10-05 00:20:49 +0900

    feat: routing navItems

--- src/router/routeConfig.tsx
+++ src/router/routeConfig.tsx
@@ -1,9 +1,18 @@
 import Home from "../pages/home/Home.tsx"
 import Login from "../pages/login/Login.tsx"
+import Purchase from "../pages/purchase/Purchase.tsx"
+import Store from "../pages/store/Store.tsx"
+import Reservation from "../pages/reservation/Reservation.tsx"
+import MyPage from "../pages/myPage/MyPage.tsx"
 
 const routeConfig = [
   { path: "/", element: <Home />, auth: true },
   { path: "/login", element: <Login />, auth: false },
+  { path: "/signup", element: <div>Signup</div>, auth: false },
+  { path: "/purchase", element: <Purchase />, auth: true },
+  { path: "/store", element: <Store />, auth: true },
+  { path: "/reservation", element: <Reservation />, auth: true },
+  { path: "/mypage", element: <MyPage />, auth: true },
 ]
 
 export default routeConfig
