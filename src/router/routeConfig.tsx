6faa4570fac694fcc01391c7b653ad91a1ea8967
Jinha <ceo@onboarders.co.kr>
2024-10-05 00:09:13 +0900

    feat: routeConfig

--- src/router/routeConfig.tsx
+++ src/router/routeConfig.tsx
@@ -1,5 +1,6 @@
 import Home from "../pages/home/Home.tsx"
 import Login from "../pages/login/Login.tsx"
+
 import Purchase from "../pages/purchase/Purchase.tsx"
 import Store from "../pages/store/Store.tsx"
 import Reservation from "../pages/reservation/Reservation.tsx"
