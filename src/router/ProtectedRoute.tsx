d0a06ce585ff823a4396ee9f0072539bcf54e040
Jinha <ceo@onboarders.co.kr>
2024-10-05 15:41:50 +0900

    refactor: paths of contexts

--- src/router/ProtectedRoute.tsx
+++ src/router/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import { useAuth } from "../auth/AuthContext.tsx"
+import { useAuth } from "../contexts/AuthContext.tsx"
 import { Navigate } from "react-router-dom"
 
 interface ProtectedRouteProps {
