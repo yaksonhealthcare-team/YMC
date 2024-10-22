2ff8bdecc927a76b9c4202fdc5f18d6f3123fc25
Seyoung Kim <yy0867@gmail.com>
2024-10-22 20:54:29 +0900

    chore: apply changed `setHeader`

--- src/pages/branch/Branch.tsx
+++ src/pages/branch/Branch.tsx
@@ -1,5 +1,40 @@
+import { useLayout } from "../../contexts/LayoutContext.tsx"
+import { useEffect } from "react"
+import { Header } from "../../components/Header"
+
 const Branch = () => {
-  return <div>Branch</div>
+  const { setHeader } = useLayout()
+
+  useEffect(() => {
+    setHeader({
+      component: <BranchHeader />,
+      display: true,
+    })
+  }, [])
+
+  return (
+    <div>
+      <p>{"Content"}</p>
+    </div>
+  )
+}
+
+const BranchHeader = () => {
+  return (
+    <Header
+      type="location"
+      title="서울 강남구 테헤란로78길 14-10"
+      onClickLocation={() => {
+        alert("Location Clicked")
+      }}
+      onClickLeft={() => {
+        alert("Left Icon Clicked")
+      }}
+      onClickRight={() => {
+        alert("Right Icon Clicked")
+      }}
+    />
+  )
 }
 
 export default Branch
