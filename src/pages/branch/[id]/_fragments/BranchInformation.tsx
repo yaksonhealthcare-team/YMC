c039eb41b1d4a093c8fcf92b0f444317fb07fcd2
Seyoung Kim <yy0867@gmail.com>
2024-11-01 01:35:39 +0900

    feat: implement layout for the branch information tab

--- src/pages/branch/[id]/_fragments/BranchInformation.tsx
+++ src/pages/branch/[id]/_fragments/BranchInformation.tsx
@@ -1,5 +1,93 @@
-const BranchInformation = () => {
-  return <div>Information</div>
+import { BranchDetail } from "../../../../types/Branch.ts"
+import ClockIcon from "@assets/icons/ClockIcon.svg?react"
+import PinIcon from "@assets/icons/PinIcon.svg?react"
+import PhoneIcon from "@assets/icons/PhoneIcon.svg?react"
+import CopyIcon from "@assets/icons/CopyIcon.svg?react"
+import { ReactNode, useState } from "react"
+import { copyToClipboard } from "../../../../utils/copyUtils.ts"
+import { Tag } from "@components/Tag.tsx"
+
+const IconSection = ({ icon, children }: { icon: ReactNode, children: ReactNode }) => (
+  <div className={"flex items-start gap-2 font-r text-14px w-full"}>
+    <div className={"h-5 content-center"}>{icon}</div>
+    {children}
+  </div>
+)
+
+const LabelSection = ({ label, type, children }: { label: string, type: "heading" | "title", children: ReactNode }) => (
+  <div className={`flex flex-col items-start ${type === "heading" ? "gap-4" : "gap-3"} text-r text-14px`}>
+    <p className={`${type === "heading" ? "font-b text-16px" : "font-sb text-14px"}`}>{label}</p>
+    {children}
+  </div>
+)
+
+const BranchInformation = ({ branch }: { branch: BranchDetail }) => {
+  const [openImageModal, setOpenImageModal] = useState(false)
+  const { weekday, saturday, holiday } = branch.operatingHours
+
+  const renderOperatingHours = ({ label, start, end }: { label: string, start: string, end: string }) => (
+    <p>{`${label} ${(start === "" || end === "") ? "정기휴무" : `${start} - ${end}`}`}</p>
+  )
+
+  return (
+    <div className={"flex flex-col p-5 gap-6"}>
+      <div className={"flex flex-col gap-4"}>
+        <div className={"flex flex-col gap-3"}>
+          <IconSection icon={<ClockIcon />}>
+            <div className={"flex flex-col gap-1"}>
+              {renderOperatingHours({ ...weekday, label: "평일" })}
+              {renderOperatingHours({ ...saturday, label: "토요일" })}
+              {renderOperatingHours({ ...holiday, label: "일요일" })}
+            </div>
+          </IconSection>
+          <IconSection icon={<PinIcon />}>
+            <p>{branch.location.address}</p>
+          </IconSection>
+          <IconSection icon={<PhoneIcon />}>
+            <p>{branch.phoneNumber}</p>
+          </IconSection>
+        </div>
+        <button className={"relative"} onClick={() => setOpenImageModal(true)}>
+          <img className={"rounded-3xl w-full h-[100px] object-cover"} src={branch.images[0]} alt={"image"} />
+          <CopyIcon className={"absolute top-2 right-3 text-white"} />
+        </button>
+      </div>
+      <div className={"h-[1px] w-full bg-gray-200"} />
+      <div className={"flex flex-col gap-6"}>
+        <LabelSection label={"오시는 길"} type={"heading"}>
+          <div className={"bg-gray-200 w-full h-48"}>
+            지도 표시
+          </div>
+          <IconSection icon={<PinIcon />}>
+            <div className={"flex w-full justify-between"}>
+              <p>{branch.location.address}</p>
+              <button
+                className={"text-tag-blue flex-shrink-0"}
+                onClick={() => copyToClipboard(branch.location.address)}
+              >
+                {"복사"}
+              </button>
+            </div>
+          </IconSection>
+        </LabelSection>
+        <LabelSection label={"일반 버스"} type={"title"}>
+          <div className={"flex flex-col gap-2"}>
+            <p>{branch.directions.bus.description}</p>
+            <div className={"flex flex-wrap gap-1"}>
+              {branch.directions.bus.routes.map((route) => <Tag type={"rect"} title={route} />)}
+            </div>
+          </div>
+        </LabelSection>
+        <LabelSection label={"지하철 안내"} type={"title"}>
+          <p>{branch.directions.subway.description}</p>
+        </LabelSection>
+        <LabelSection label={"자동차 안내"} type={"title"}>
+          <p>{branch.directions.car.description}</p>
+        </LabelSection>
+      </div>
+      <div className={"h-4"} />
+    </div>
+  )
 }
 
 export default BranchInformation
