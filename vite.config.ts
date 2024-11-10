320c1b74556588321c6dd8aaee1291f5f50c1cfb
Jinha <ceo@onboarders.co.kr>
2024-11-10 20:29:17 +0900

    chore: Configure Vite with essential plugins and path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react-swc"
-import tsconfigPaths from "vite-tsconfig-paths"
-import svgr from "vite-plugin-svgr"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
+  resolve: {
+    alias: {
+      "@assets": "/src/assets",
+      "@components": "/src/components",
+      "@hooks": "/src/hooks",
+      "@pages": "/src/pages",
+      "@router": "/src/router",
+      "@utils": "/src/utils",
+    },
+  },
 })
