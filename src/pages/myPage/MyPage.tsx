32786ca7d75ffc763b82619e73798b3b1863a398
Jinha <ceo@onboarders.co.kr>
2024-11-14 13:45:11 +0900

    feat: logout link

--- src/pages/myPage/MyPage.tsx
+++ src/pages/myPage/MyPage.tsx
@@ -144,7 +144,12 @@ const MyPage = () => {
             <div className="flex items-center gap-3">
               <span className="font-sb text-14px text-gray-400">이용약관</span>
               <div className="w-[1px] h-3.5 bg-gray-200" />
-              <span className="font-sb text-14px text-gray-400">로그아웃</span>
+              <span
+                onClick={() => navigate("/logout")}
+                className="font-sb text-14px text-gray-400"
+              >
+                로그아웃
+              </span>
             </div>
             <span className="font-r text-12px text-gray-300">
               v.1.1.4 version
