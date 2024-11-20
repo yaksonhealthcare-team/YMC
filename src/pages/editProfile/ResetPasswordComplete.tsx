132ecd9918423c613556a80b18dd23896a848d92
Seyoung Kim <yy0867@gmail.com>
2024-11-20 23:10:12 +0900

    feat: implement reset password ui

---
+++ src/pages/editProfile/ResetPasswordComplete.tsx
@@ -0,0 +1,54 @@
+import { useAuth } from "../../contexts/AuthContext.tsx"
+import { useLayout } from "../../contexts/LayoutContext.tsx"
+import { useNavigate } from "react-router-dom"
+import { useEffect } from "react"
+import CaretLeftIcon from "@assets/icons/CaretLeftIcon.svg?react"
+import CheckCircle from "@assets/icons/CheckCircle.svg?react"
+import { Button } from "@components/Button.tsx"
+
+const ResetPasswordComplete = () => {
+  const { logout } = useAuth()
+  const { setHeader, setNavigation } = useLayout()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    setHeader({ display: false })
+    setNavigation({ display: false })
+  }, [])
+
+  const navigateToLogin = () => {
+    logout()
+    navigate("/login")
+  }
+
+  return (
+    <div className={"flex flex-col w-full h-full"}>
+      <button className={"px-5 py-4"} onClick={navigateToLogin}>
+        <CaretLeftIcon className={"w-5 h-5"} />
+      </button>
+      <div
+        className={
+          "flex flex-col h-full w-full items-stretch justify-center p-5"
+        }
+      >
+        <div className="self-center w-[60px] h-[60px] flex items-center justify-center">
+          <CheckCircle className="w-[60px] h-[60px] text-primary" />
+        </div>
+        <p className={"font-sb text-20px text-center mt-7"}>
+          {"비밀번호가 변경되었어요"}
+          <br />
+          {"다시 로그인해주세요"}
+        </p>
+        <Button
+          className={"mt-10"}
+          variantType={"primary"}
+          onClick={navigateToLogin}
+        >
+          로그인 페이지로 이동
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default ResetPasswordComplete
