9440a1e2f275a320448945b14bf8fd305e7c3cd2
Seyoung Kim <yy0867@gmail.com>
2024-11-23 23:45:22 +0900

    chore: implement default UI

--- src/pages/payment/PaymentPage.tsx
+++ src/pages/payment/PaymentPage.tsx
@@ -1,4 +1,19 @@
+import { useLayout } from "../../contexts/LayoutContext.tsx"
+import { useEffect } from "react"
+
 const PaymentPage = () => {
+  const { setHeader, setNavigation } = useLayout()
+
+  useEffect(() => {
+    setHeader({
+      display: true,
+      left: "back",
+      title: "결제 내역",
+      backgroundColor: "bg-white",
+    })
+    setNavigation({ display: false })
+  }, [])
+
   return (
     <div>
       <h1>Payments</h1>
