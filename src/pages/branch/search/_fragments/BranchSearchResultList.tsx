8213c69192bc59352838011904c75f9e21672303
Seyoung Kim <yy0867@gmail.com>
2024-11-26 22:09:46 +0900

    chore: fix query data to infinite query type

--- src/pages/branch/search/_fragments/BranchSearchResultList.tsx
+++ src/pages/branch/search/_fragments/BranchSearchResultList.tsx
@@ -4,6 +4,7 @@ import useDebounce from "../../../../hooks/useDebounce.tsx"
 import { useBranches } from "../../../../queries/useBranchQueries.tsx"
 import useGeolocation from "../../../../hooks/useGeolocation.tsx"
 import { DEFAULT_COORDINATE } from "../../../../types/Coordinate.ts"
+import useIntersection from "../../../../hooks/useIntersection.tsx"
 
 interface BranchSearchResultListProps {
   query: string
@@ -13,8 +14,13 @@ const BranchSearchResultList = ({ query }: BranchSearchResultListProps) => {
   const navigate = useNavigate()
   const debouncedQuery = useDebounce(query, 300)
   const { location } = useGeolocation()
-
-  const { data: branches, isLoading } = useBranches({
+  const {
+    data: branches,
+    isLoading,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useBranches({
     page: 1,
     latitude: location.latitude ?? DEFAULT_COORDINATE.latitude!,
     longitude: location.longitude ?? DEFAULT_COORDINATE.longitude!,
@@ -22,13 +28,21 @@ const BranchSearchResultList = ({ query }: BranchSearchResultListProps) => {
     search: debouncedQuery,
   })
 
-  if (!isLoading && branches?.length === 0) {
+  const { observerTarget } = useIntersection({
+    onIntersect: () => {
+      if (hasNextPage && !isFetchingNextPage) {
+        fetchNextPage()
+      }
+    },
+  })
+
+  if (!isLoading && branches?.pages.flatMap((page) => page).length === 0) {
     return <p className={"self-center mt-40"}>{"검색 결과가 없습니다."}</p>
   }
 
   return (
     <ul className={"divide-y divide-gray-100 px-5 overflow-y-scroll"}>
-      {(branches || []).map((branch, index) => (
+      {(branches?.pages.flatMap((page) => page) || []).map((branch, index) => (
         <li
           key={index}
           className={"py-4"}
@@ -39,6 +53,7 @@ const BranchSearchResultList = ({ query }: BranchSearchResultListProps) => {
           <BranchCard name={branch.name} address={branch.address} />
         </li>
       ))}
+      <div ref={observerTarget} className={"h-4"} />
     </ul>
   )
 }
