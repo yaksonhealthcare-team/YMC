414322a832ec8d71fd742fa061ad0ecfea38f5b3
Seyoung Kim <yy0867@gmail.com>
2024-12-01 01:19:58 +0900

    feat: implement event & notice queries

---
+++ src/queries/useContentQueries.tsx
@@ -0,0 +1,21 @@
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query"
+import { queryKeys } from "./query.keys.ts"
+import { fetchEvent, fetchEvents } from "../apis/contents.api.ts"
+import { EventStatus } from "../types/Content.ts"
+
+export const useEvents = (status: EventStatus) =>
+  useInfiniteQuery({
+    initialPageParam: 1,
+    queryKey: queryKeys.events.list({ page: 1, status }),
+    queryFn: ({ pageParam = 1 }) => fetchEvents(status, pageParam),
+    getNextPageParam: (lastPage, allPages) => {
+      if (lastPage.length === 0) return undefined
+      return allPages.length + 1
+    },
+  })
+
+export const useEvent = (id: string) =>
+  useQuery({
+    queryKey: queryKeys.events.detail(id),
+    queryFn: () => fetchEvent(id),
+  })
