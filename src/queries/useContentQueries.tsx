1a9ac1169f4edbfb137211a35da694b67ae13597
Seyoung Kim <yy0867@gmail.com>
2024-12-01 01:34:10 +0900

    feat: add notice queries

--- src/queries/useContentQueries.tsx
+++ src/queries/useContentQueries.tsx
@@ -1,6 +1,11 @@
 import { useInfiniteQuery, useQuery } from "@tanstack/react-query"
 import { queryKeys } from "./query.keys.ts"
-import { fetchEvent, fetchEvents } from "../apis/contents.api.ts"
+import {
+  fetchEvent,
+  fetchEvents,
+  fetchNotice,
+  fetchNotices,
+} from "../apis/contents.api.ts"
 import { EventStatus } from "../types/Content.ts"
 
 export const useEvents = (status: EventStatus) =>
@@ -18,4 +23,21 @@ export const useEvent = (id: string) =>
   useQuery({
     queryKey: queryKeys.events.detail(id),
     queryFn: () => fetchEvent(id),
+  })
+
+export const useNotices = () =>
+  useInfiniteQuery({
+    initialPageParam: 1,
+    queryKey: queryKeys.notices.list({ page: 1 }),
+    queryFn: ({ pageParam = 1 }) => fetchNotices(pageParam),
+    getNextPageParam: (lastPage, allPages) => {
+      if (lastPage.length === 0) return undefined
+      return allPages.length + 1
+    },
+  })
+
+export const useNotice = (id: string) =>
+  useQuery({
+    queryKey: queryKeys.notices.detail(id),
+    queryFn: () => fetchNotice(id),
   })
