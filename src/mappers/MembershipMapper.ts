e2d38ce58e53c1fa0d1afc5cc324b374cc72531d
thumper-lee <thumper@onboarders.co.kr>
2024-12-03 00:28:55 +0900

    feat: Add membership detail types and mapper

--- src/mappers/MembershipMapper.ts
+++ src/mappers/MembershipMapper.ts
@@ -1,8 +1,14 @@
 import {
   Membership,
+  MembershipDetail,
+  MembershipDetailResponse,
+  MembershipOption,
+  MembershipOptionResponse,
   MembershipResponse,
   ServiceCategory,
   ServiceCategoryResponse,
+  ServiceCourse,
+  ServiceCourseResponse,
 } from "types/Membership"
 
 export class ServiceCategoryMapper {
@@ -21,6 +27,36 @@ export class ServiceCategoryMapper {
   }
 }
 
+export class ServiceCourseMapper {
+  static toEntity(dto: ServiceCourseResponse): ServiceCourse {
+    return {
+      serviceCourseIndex: dto.sc_idx,
+      serviceCourseName: dto.sc_name,
+      serviceCourseMinutes: dto.sc_min,
+      priority: dto.prior,
+    }
+  }
+
+  static toEntities(dtos: ServiceCourseResponse[]): ServiceCourse[] {
+    return dtos.map(this.toEntity)
+  }
+}
+
+export class MembershipOptionMapper {
+  static toEntity(dto: MembershipOptionResponse): MembershipOption {
+    return {
+      subscriptionIndex: dto.ss_idx,
+      subscriptionCount: dto.ss_count,
+      subscriptionOriginalPrice: dto.original_price,
+      subscriptionPrice: dto.ss_price,
+    }
+  }
+
+  static toEntities(dtos: MembershipOptionResponse[]): MembershipOption[] {
+    return dtos.map(this.toEntity)
+  }
+}
+
 export class MembershipMapper {
   static toEntity(dto: MembershipResponse): Membership {
     return {
@@ -29,16 +65,26 @@ export class MembershipMapper {
       brandName: dto.brand_name,
       serviceTime: dto.s_time,
       serviceType: dto.s_type,
-      options: dto.options.map((option) => ({
-        subscriptionIndex: option.ss_idx,
-        subscriptionCount: option.ss_count,
-        subscriptionOriginalPrice: option.original_price,
-        subscriptionPrice: option.ss_price,
-      })),
+      options: MembershipOptionMapper.toEntities(dto.options),
     }
   }
 
   static toEntities(dtos: MembershipResponse[]): Membership[] {
     return dtos.map(this.toEntity)
+  }
+}
+
+export class MembershipDetailMapper {
+  static toEntity(dto: MembershipDetailResponse): MembershipDetail {
+    return {
+      serviceName: dto.s_name,
+      brandName: dto.brand_name || "",
+      serviceContent: dto.s_content || "",
+      serviceTime: dto.s_time,
+      serviceType: dto.s_type,
+      courses: ServiceCourseMapper.toEntities(dto.courses),
+      pictures: dto.pictures,
+      options: MembershipOptionMapper.toEntities(dto.options),
+    }
   }
 }
