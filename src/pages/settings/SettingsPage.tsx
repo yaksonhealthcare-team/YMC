ffb706ac2052e46ff84f07e94c99d8469146f082
Seyoung Kim <yy0867@gmail.com>
2024-12-04 21:34:50 +0900

    feat: Implement notification settings page

--- src/pages/settings/SettingsPage.tsx
+++ src/pages/settings/SettingsPage.tsx
@@ -1,7 +1,80 @@
+import { useLayout } from "../../contexts/LayoutContext.tsx"
+import { useEffect, useState } from "react"
+import Switch from "@components/Switch.tsx"
+
 const SettingsPage = () => {
+  const { setHeader, setNavigation } = useLayout()
+
+  const [notifications, setNotifications] = useState({
+    reservations: true,
+    payments: false,
+    points: true,
+    notices: true,
+  })
+
+  useEffect(() => {
+    setHeader({
+      left: "back",
+      title: "알림설정",
+      backgroundColor: "bg-white",
+      display: true,
+    })
+    setNavigation({ display: false })
+  }, [])
+
   return (
-    <div>
-      <h1>Settings</h1>
+    <div className={"flex flex-col px-5 divide-y divide-gray-100"}>
+      <div className={"flex justify-between py-6"}>
+        <p>{"예약 알림"}</p>
+        <Switch.IOS
+          checked={notifications.reservations}
+          onChange={(e) =>
+            setNotifications((prev) => ({
+              ...prev,
+              reservations: e.target.checked,
+            }))
+          }
+        />
+      </div>
+
+      <div className={"flex justify-between py-6"}>
+        <p>{"결제 알림"}</p>
+        <Switch.IOS
+          checked={notifications.payments}
+          onChange={(event) =>
+            setNotifications((prev) => ({
+              ...prev,
+              payments: event.target.checked,
+            }))
+          }
+        />
+      </div>
+
+      <div className={"flex justify-between py-6"}>
+        <p>{"포인트 알림"}</p>
+        <Switch.IOS
+          checked={notifications.points}
+          onChange={(event) =>
+            setNotifications((prev) => ({
+              ...prev,
+              points: event.target.checked,
+            }))
+          }
+        />
+      </div>
+
+      <div className={"flex justify-between py-6"}>
+        <p>{"공지 알림"}</p>
+        <Switch.IOS
+          checked={notifications.notices}
+          onChange={(event) =>
+            setNotifications((prev) => ({
+              ...prev,
+              notices: event.target.checked,
+            }))
+          }
+        />
+      </div>
     </div>
   )
 }
