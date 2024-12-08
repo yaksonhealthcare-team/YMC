f6a1dd65d3e63419695b8a71bdca9fb1136db627
xoals25 <xoals25@naver.com>
2024-12-09 00:41:50 +0900

    design: crm search select list

--- src/pages/addUsingBranch/AddUsingBranch.tsx
+++ src/pages/addUsingBranch/AddUsingBranch.tsx
@@ -5,15 +5,15 @@ import CustomTextField from "@components/CustomTextField.tsx"
 import { IconButton } from "@mui/material"
 import SearchIcon from "@components/icons/SearchIcon.tsx"
 import CloseGrayFillIcon from "@assets/icons/CloseGrayFillIcon.svg?react"
-import BranchIcon from "@assets/icons/BranchIcon.svg?react"
 import CheckIcon from "@assets/icons/CheckIcon.svg?react"
 import { Button } from "@components/Button.tsx"
+import BranchIcon from "@components/icons/BranchIcon.tsx"
 
 const SEARCH_HISTORY_KEY = "search/branches"
 
 interface Branch {
-  id: number,
-  name: string,
+  id: number
+  name: string
   address: string
 }
 
@@ -46,6 +46,38 @@ const MOCK_BRANCHES: Branch[] = [
   },
   {
     id: 125,
+    name: "약손명가 서울대입구역점",
+    address:
+      "서울 관악구 관악로 195 (봉천동, 관악위버폴리스) 제3층 306호, 307호",
+  },
+  {
+    id: 1502,
+    name: "달리아에스테틱 건대점",
+    address: "서울 광진구 아차산로34길 26 (자양동) 4층",
+  },
+  {
+    id: 1232,
+    name: "약손명가 테스트지점",
+    address: "서울 강남구 영동대로142길 27 (청담동) 3층",
+  },
+  {
+    id: 1692,
+    name: "약손명가 상봉역점",
+    address:
+      "서울 중랑구 망우로 353 (상봉동, 상봉 프레미어스 엠코) C동 303호, 304호",
+  },
+  {
+    id: 1682,
+    name: "약손명가 헬스케어",
+    address: "서울 강남구 봉은사로68길 8 (삼성동, DECKS 빌딩) 2층",
+  },
+  {
+    id: 1622,
+    name: "약손명가 당산역점",
+    address: "서울 영등포구 양평로 20 (당산동6가) 4층",
+  },
+  {
+    id: 1252,
     name: "약손명가 서울대입구역점",
     address:
       "서울 관악구 관악로 195 (봉천동, 관악위버폴리스) 제3층 306호, 307호",
@@ -122,8 +154,8 @@ const AddUsingBranch = () => {
 
     const updatedSearchHistories = storedHistory
       ? JSON.parse(storedHistory).filter(
-        (item: string) => item !== itemToRemove,
-      )
+          (item: string) => item !== itemToRemove,
+        )
       : []
 
     localStorage.setItem(
@@ -144,7 +176,7 @@ const AddUsingBranch = () => {
 
   return (
     <>
-      {pageStep === 1 &&
+      {pageStep === 1 && (
         <div className="flex flex-col justify-between h-full">
           <div className="px-[20px] mt-[20px]">
             <p className="text-gray-700 font-bold text-20px">
@@ -226,19 +258,62 @@ const AddUsingBranch = () => {
                   </div>
                 </div>
 
-                {selectedBranches.some((b) => b.id === branch.id) && <CheckIcon />}
+                {selectedBranches.some((b) => b.id === branch.id) && (
+                  <CheckIcon />
+                )}
               </div>
             ))}
           </div>
-
         </div>
-      }
-      
+      )}
+      {pageStep === 2 && (
+        <div className="flex flex-col justify-between h-full">
+          <div className="px-[20px] mt-[20px]">
+            <p className="text-gray-700 font-bold text-20px">
+              총{" "}
+              <span className="text-primary-300">
+                {selectedBranches.length}개
+              </span>
+              의 지점을
+              <br />
+              이용하고 계시군요!
+            </p>
+            <p className="mt-[12px] text-gray-400">
+              이용중이신 지점과 회원 정보 연동을 진행해주세요
+            </p>
+          </div>
+
+          <div className="flex-auto h-[0px] min-h-[200px] px-[20px] overflow-y-auto mt-[40px]">
+            {selectedBranches.map((branch, index) => (
+              <div
+                key={branch.id}
+                className={`p-[20px] border border-primary-300 rounded-[20px] ${index !== 0 && "mt-[16px]"}`}
+              >
+                <div className="flex gap-[6px]">
+                  <BranchIcon className="w-[16px] h-[16px] mt-[3px]" />
+                  <div className="text-[14px]">
+                    <p className="font-bold text-gray-700">{branch.name}</p>
+                    <p className="font-normal text-gray-500 mt-[8px]">
+                      {branch.address}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+
       <div className="w-full px-[20px] pt-[12px] pb-[30px] bg-[#FFFFFF] border-t border-[#F8F8F8]">
-        <Button className="w-full" onClick={handleNextStep}>다음</Button>
+        <Button className="w-full" onClick={handleNextStep}>
+          {pageStep === 1
+            ? "다음"
+            : pageStep === 2
+              ? "회원 정보 연동하기"
+              : "문진 작성하기"}
+        </Button>
       </div>
     </>
-
   )
 }
 
