32dcfe8d0cccc832dcb449362d7bc8cf19165f0b
xoals25 <xoals25@naver.com>
2024-12-09 00:42:24 +0900

    design: crm finish

--- src/pages/addUsingBranch/AddUsingBranch.tsx
+++ src/pages/addUsingBranch/AddUsingBranch.tsx
@@ -304,7 +304,30 @@ const AddUsingBranch = () => {
         </div>
       )}
 
+      {pageStep === 3 && (
+        <div className="h-full">
+          <div className="flex flex-col justify-center items-center px-[20px] pt-[20px] pb-[24px] mt-[100px]">
+            <BranchIcon
+              color="#F37165"
+              className="w-[60px] h-[60px] mt-[3px]"
+            />
+
+            <p className="mt-[28px] text-center font-semibold text-[20px]">
+              회원정보 연동까지는
+              <br />
+              2~3일 정도 소요될 수 있습니다
+            </p>
+          </div>
+        </div>
+      )}
+
       <div className="w-full px-[20px] pt-[12px] pb-[30px] bg-[#FFFFFF] border-t border-[#F8F8F8]">
+        {pageStep === 3 && (
+          <button className="w-full bg-transparent text-primary-300 mb-[8px] h-[48px] font-semibold text-[16px]">
+            나중에 등록할래요
+          </button>
+        )}
+
         <Button className="w-full" onClick={handleNextStep}>
           {pageStep === 1
             ? "다음"
