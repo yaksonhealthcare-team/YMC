353d7f905bef2c6a257823ebe75d7c0cf1b9cf45
xoals25 <xoals25@naver.com>
2024-12-11 16:34:52 +0900

    feat: branch search list infinite scroll

---
+++ src/pages/addUsingBranch/BranchItem.tsx
@@ -0,0 +1,22 @@
+import BranchIcon from "@components/icons/BranchIcon.tsx"
+import { Branch } from "../../types/Branch.ts"
+
+interface BranchItemProps {
+  branch: Branch
+}
+
+const BranchItem = ({ branch }: BranchItemProps) => {
+  return (
+    <>
+      <div className="flex gap-[6px]">
+        <BranchIcon className="w-[16px] h-[16px] mt-[3px]" />
+        <div className="text-[14px]">
+          <p className="font-bold text-gray-700">{branch.name}</p>
+          <p className="font-normal text-gray-500 mt-[8px]">{branch.address}</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default BranchItem
