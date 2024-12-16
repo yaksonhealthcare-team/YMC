06226b5b9b2e1e3df228f59662829ce7eb9d78cf
Seyoung Kim <yy0867@gmail.com>
2024-12-16 22:46:25 +0900

    chore: Move selected bottom sheet UI into BranchMapSection

--- src/pages/branch/_fragments/BranchMapSection.tsx
+++ src/pages/branch/_fragments/BranchMapSection.tsx
@@ -1,26 +1,97 @@
 import MapView from "@components/MapView.tsx"
 import { Branch } from "../../../types/Branch.ts"
+import { Coordinate, DEFAULT_COORDINATE } from "../../../types/Coordinate.ts"
+import { useEffect, useState } from "react"
+import { fetchBranches } from "../../../apis/branch.api.ts"
+import { BranchFilterListItem } from "./BranchFilterList.tsx"
+import {
+  useBranchBookmarkMutation,
+  useBranchUnbookmarkMutation,
+} from "../../../queries/useBranchQueries.tsx"
 
 interface BranchMapSectionProps {
-  branches: Branch[]
+  brandCode?: string
+  category?: string
   onSelectBranch: (branch: Branch | null) => void
 }
 
 const BranchMapSection = ({
-  branches,
+  brandCode,
+  category,
   onSelectBranch,
 }: BranchMapSectionProps) => {
-  const markers = branches.map((branch) => {
-    return new naver.maps.Marker({
-      position: new naver.maps.LatLng(branch.latitude, branch.longitude),
-      map,
-      clickable: true,
+  const [branches, setBranches] = useState<Branch[]>([])
+  const [coords, setCoords] = useState<Coordinate>(DEFAULT_COORDINATE)
+  const [branchForRender, setBranchForRender] = useState<Branch | null>(null)
+  const [selectedBranch, setSelectedBranch] = useState<Branch | null>(null)
+
+  const { mutateAsync: addBookmark } = useBranchBookmarkMutation()
+  const { mutateAsync: removeBookmark } = useBranchUnbookmarkMutation()
+
+  useEffect(() => {
+    fetchBranchesByCoords(coords)
+  }, [])
+
+  const fetchBranchesByCoords = async (coords: Coordinate) => {
+    setCoords(coords)
+    const result = await fetchBranches({
+      page: 1,
+      latitude: coords.latitude,
+      longitude: coords.longitude,
+      brandCode,
+      category,
     })
-  })
+    setBranches((prev) => {
+      return [
+        ...prev.filter(
+          (branch) => !result.some((newBranch) => newBranch.id === branch.id),
+        ),
+        ...result,
+      ]
+    })
+    if (selectedBranch) {
+      const newBranch = result.find((branch) => selectedBranch.id === branch.id)
+      if (newBranch) {
+        setSelectedBranch(newBranch)
+        setBranchForRender(newBranch)
+      }
+    }
+  }
 
   return (
-    <div className={"flex flex-col flex-1 h-full"}>
-      <MapView />
+    <div className={"relative flex flex-col flex-1 h-full overflow-hidden"}>
+      <MapView
+        branches={branches || []}
+        options={{
+          onSelectBranch: (branch) => {
+            setSelectedBranch(branch)
+            if (branch) setBranchForRender(branch)
+            onSelectBranch?.(branch)
+          },
+          onMoveMap: fetchBranchesByCoords,
+          showCurrentLocationButton: true,
+        }}
+      />
+      <div
+        className={`w-full absolute px-5 pb-6 pt-2 -bottom-32 rounded-t-3xl ${selectedBranch ? "transition-transform -translate-y-32 duration-300" : "transition-transform translate-y-6 duration-300"} bg-white z-[300]`}
+      >
+        {branchForRender ? (
+          <BranchFilterListItem
+            branch={branchForRender}
+            onClick={() => {}}
+            onClickFavorite={async (branch) => {
+              if (branch.isFavorite) {
+                await removeBookmark(branch.id)
+              } else {
+                await addBookmark(branch.id)
+              }
+              await fetchBranchesByCoords(coords)
+            }}
+          />
+        ) : (
+          <div className={"w-full h-20"} />
+        )}
+      </div>
     </div>
   )
 }
