f716bde4c64b90e3f40c0845cd9086f329f45c6d
Jinha <58817064+jinha-ryu@users.noreply.github.com>
2024-12-18 13:54:02 +0900

    fix: build error

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,34 @@
-import OAuthCallback from "../pages/oauth/OAuthCallback"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import ProtectedRoute from "./ProtectedRoute.tsx"
+import { AuthProvider } from "../contexts/AuthContext.tsx"
+import { LayoutProvider } from "../contexts/LayoutContext.tsx"
+import routeConfig from "./routeConfig.tsx"
+import { SignupProvider } from "../contexts/SignupContext.tsx"
 
-{
-  path: '/oauth/callback/:provider',
-  element: <OAuthCallback />,
-} 
+const createRoutes = () => {
+  return createBrowserRouter(
+    routeConfig.map(({ path, element, auth, children }) => {
+      if (path.startsWith("/signup")) {
+        element = <SignupProvider>{element}</SignupProvider>
+      }
+
+      if (auth) {
+        element = <ProtectedRoute>{element}</ProtectedRoute>
+      }
+
+      element = <LayoutProvider>{element}</LayoutProvider>
+
+      return {
+        path,
+        element,
+        children,
+      }
+    }),
+  )
+}
+
+export const AppRouter = () => (
+  <AuthProvider>
+    <RouterProvider router={createRoutes()} />
+  </AuthProvider>
+)
