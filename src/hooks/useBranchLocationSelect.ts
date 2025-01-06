f143d78e14f7edda71e33f56363fbb8444e35780
Seyoung Kim <yy0867@gmail.com>
2025-01-07 02:04:10 +0900

    feat: implement location picker page

---
+++ src/hooks/useBranchLocationSelect.ts
@@ -0,0 +1,17 @@
+import { Coordinate } from "../types/Coordinate.ts"
+import { create } from "zustand"
+
+interface BranchLocationSelect {
+  location: { address: string; coords: Coordinate } | null
+  setLocation: (location: { address: string; coords: Coordinate }) => void
+  clear: () => void
+}
+
+export const useBranchLocationSelect = create<BranchLocationSelect>((set) => ({
+  location: null,
+  setLocation: ({ address, coords }) =>
+    set({
+      location: { address, coords },
+    }),
+  clear: () => set({ location: null }),
+}))
