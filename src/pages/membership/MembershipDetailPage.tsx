2366d636b3d9d1f2a934b0a086320845e41fbdef
Jinha <58817064+jinha-ryu@users.noreply.github.com>
2025-01-24 01:12:29 +0900

    refactor: remove unnecessary clearMembershipOptions call in MembershipDetailPage
    - Eliminated the clearMembershipOptions function call from the MembershipDetailPage component to streamline navigation logic.
    - This change enhances the user experience by simplifying the interaction when navigating back from the membership details.

--- src/pages/membership/MembershipDetailPage.tsx
+++ src/pages/membership/MembershipDetailPage.tsx
@@ -58,7 +58,6 @@ const MembershipDetailPage = () => {
         <div className={"flex items-center justify-between px-5 py-3 h-[48px]"}>
           <div
             onClick={() => {
-              clearMembershipOptions()
               navigate(-1)
             }}
           >
