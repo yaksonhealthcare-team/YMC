30988abafbb7b9de65921be13adec1e2bf502095
dev-n0v4 <nova@onboarders.co.kr>
2025-01-27 20:57:05 +0900

    feat: Make return_url dynamic based on current environment for PASS authentication

--- src/apis/pass.api.ts
+++ src/apis/pass.api.ts
@@ -9,9 +9,14 @@ export interface EncryptData {
 }
 
 export const fetchEncryptDataForNice = async () => {
+  // TODO: 프로덕션 배포 시 return_url 도메인 수정 필요
+  // 현재: 개발환경(localhost) 및 개발서버에서 사용
+  const origin = window.location.origin
+  const return_url = `${origin}/signup/callback`
+
   const { data } = await axiosClient.post<HTTPResponse<EncryptData[]>>(
     "/auth/crypto/token.php",
-    { return_url: "http://localhost:5173/signup/callback" },
+    { return_url },
   )
 
   return data
