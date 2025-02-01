03aa73930521f7ce0013dceb9a7c4a9b15f830cb
dev-n0v4 <nova@onboarders.co.kr>
2025-02-02 04:28:55 +0900

    refactor: Restructure membership history page and add filter feature

--- src/pages/member-history/membership/MembershipHistoryPage.tsx
+++ src/pages/member-history/membership/MembershipHistoryPage.tsx
@@ -1,14 +1,17 @@
 import { useLayout } from "../../../contexts/LayoutContext"
-import { useEffect } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { useUserMemberships } from "../../../queries/useMembershipQueries"
 import useIntersection from "../../../hooks/useIntersection"
 import LoadingIndicator from "@components/LoadingIndicator"
 import { useNavigate } from "react-router-dom"
 import { MembershipCard } from "@components/MembershipCard"
-import { MembershipStatus } from "../../../types/Membership"
+import { MembershipStatus, myMembershipFilters, MyMembershipFilterItem } from "../../../types/Membership"
+import MainTabs from "../../memberHistory/_fragments/MainTabs"
+import { Button } from "@components/Button"
+import clsx from "clsx"
+import ReservationIcon from "@assets/icons/ReservationIcon.svg?react"
 
-const MembershipHistoryPage = () => {
-  const { setHeader, setNavigation } = useLayout()
+const MembershipContent = ({ filterId }: { filterId: string }) => {
   const navigate = useNavigate()
   const {
     data: memberships,
@@ -16,7 +19,7 @@ const MembershipHistoryPage = () => {
     hasNextPage,
     isFetchingNextPage,
     isLoading,
-  } = useUserMemberships()
+  } = useUserMemberships(filterId === "-" ? "" : filterId)
 
   const { observerTarget } = useIntersection({
     onIntersect: () => {
@@ -26,49 +29,115 @@ const MembershipHistoryPage = () => {
     },
   })
 
-  useEffect(() => {
-    setHeader({
-      display: true,
-      title: "회원권 내역",
-      left: "back",
-      backgroundColor: "bg-white",
-    })
-    setNavigation({ display: false })
-  }, [])
-
   if (isLoading) {
-    return <LoadingIndicator className="min-h-screen" />
+    return <LoadingIndicator className="flex-1" />
   }
 
-  if (!memberships?.pages) return null
-
   return (
-    <div className="flex flex-col">
-      {memberships.pages.map((page) =>
-        page.body.map((membership) => (
-          <div
-            key={membership.mp_idx}
-            className="flex flex-col gap-4 p-5 border-b border-gray-100"
-            onClick={() => navigate(`/membership/usage/${membership.mp_idx}`)}
-          >
-            <MembershipCard
-              id={parseInt(membership.mp_idx)}
-              title={membership.service_name || '회원권 이름'}
-              count={`${membership.remain_amount}회 / ${membership.buy_amount}회`}
-              date={`${membership.pay_date.split(" ")[0]} - ${membership.expiration_date.split(" ")[0]}`}
-              status={membership.status as MembershipStatus}
-              showReserveButton={true}
-              serviceType={membership.s_type.replace('회원권', '').trim()}
-            />
-          </div>
-        ))
-      )}
-      <div ref={observerTarget} className="h-4" />
-      {isFetchingNextPage && (
-        <LoadingIndicator className="min-h-[100px]" />
+    <div className="flex-1 px-5 space-y-3 pb-32 overflow-y-auto scrollbar-hide">
+      {!memberships?.pages[0].body?.length ? (
+        <div className="flex justify-center items-center p-4">
+          회원권 내역이 없습니다.
+        </div>
+      ) : (
+      <div className="space-y-3">
+        {memberships.pages.map((page) =>
+          page.body.map((membership) => (
+            <div
+              key={membership.mp_idx}
+              onClick={() => navigate(`/membership/usage/${membership.mp_idx}`)}
+            >
+              <MembershipCard
+                id={parseInt(membership.mp_idx)}
+                title={membership.service_name || '회원권 이름'}
+                count={`${membership.remain_amount}회 / ${membership.buy_amount}회`}
+                date={`${membership.pay_date.split(" ")[0]} - ${membership.expiration_date.split(" ")[0]}`}
+                status={membership.status as MembershipStatus}
+                showReserveButton={true}
+                serviceType={membership.s_type.replace('회원권', '').trim()}
+              />
+            </div>
+          ))
+        )}
+        <div ref={observerTarget} className="h-4" />
+        {isFetchingNextPage && (
+          <LoadingIndicator className="min-h-[100px]" />
+        )}
+      </div>
       )}
     </div>
   )
 }
 
-export default MembershipHistoryPage 
+const FilterContent = ({ 
+  membershipFilter, 
+  onFilterChange 
+}: { 
+  membershipFilter: MyMembershipFilterItem
+  onFilterChange: (filter: MyMembershipFilterItem) => void
+}) => {
+  return (
+    <div className="px-5 py-4 flex justify-center gap-2">
+      {myMembershipFilters.map((filter) => {
+        const isSelected = filter.id === membershipFilter.id
+        return (
+          <Button
+            key={filter.id}
+            fullCustom
+            className={clsx(
+              "min-w-0 whitespace-nowrap px-[12px] py-[5px] text-12px font-sb !rounded-full",
+              isSelected
+                ? "bg-primary-50 text-primary border border-solid border-primary"
+                : "bg-white text-gray-500 border border-solid border-gray-200",
+            )}
+            onClick={() => onFilterChange(filter)}
+          >
+            {filter.title}
+          </Button>
+        )
+      })}
+    </div>
+  )
+}
+
+const MembershipHistoryPage = () => {
+  const navigate = useNavigate()
+  const { setHeader, setNavigation } = useLayout()
+  const [membershipFilter, setMembershipFilter] = useState<MyMembershipFilterItem>(
+    myMembershipFilters[0],
+  )
+
+  const handleFilterChange = useCallback((filter: MyMembershipFilterItem) => {
+    setMembershipFilter(filter)
+  }, [])
+
+  useEffect(() => {
+    setHeader({
+      display: false,
+    })
+    setNavigation({ display: true })
+  }, [])
+
+  return (
+    <div className="flex flex-col bg-system-bg min-h-[calc(100vh-82px)]">
+      <div className="px-5">
+        <MainTabs />
+      </div>
+
+      <FilterContent 
+        membershipFilter={membershipFilter} 
+        onFilterChange={handleFilterChange} 
+      />
+
+      <MembershipContent filterId={membershipFilter.id} />
+      <button
+        className="fixed bottom-[98px] right-5 w-14 h-14 bg-primary-300 text-white rounded-full shadow-lg hover:bg-primary-400 focus:outline-none focus:bg-primary-500 focus:ring-opacity-50 transition-colors duration-200 z-10"
+        onClick={() => navigate("/membership")}
+      >
+        <ReservationIcon className="w-8 h-8 mx-auto text-white" />
+      </button>
+    </div>
+  )
+}
+
+export default MembershipHistoryPage
