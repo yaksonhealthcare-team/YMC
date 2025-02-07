b87284002beb7eb8794f9b0418ea7b71721e8fde
thumper-lee <thumper@onboarders.co.kr>
2025-02-07 16:30:05 +0900

    feat: Add Empty reservation button

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -187,13 +187,11 @@ const Home = () => {
 const ReserveCardSection = () => {
   const { data: upcomingReservations } = useUpcomingReservations()
   const navigate = useNavigate()
+  const { clear } = useMembershipOptionsStore()
 
-  if (!upcomingReservations || upcomingReservations.length === 0) {
-    return (
-      <EmptyCard
-        title={`예정된 예약이 없어요.\n예약을 통해 관리를 받아보세요.`}
-      />
-    )
+  const handleReservationClick = () => {
+    clear()
+    navigate("/reservation/form")
   }
 
   return (
@@ -201,21 +199,29 @@ const ReserveCardSection = () => {
       <Title
         type="arrow"
         title="예정된 예약"
-        count={`${upcomingReservations.length}건`}
+        count={upcomingReservations?.length ? `${upcomingReservations.length}건` : "0건"}
         onClick={() => navigate("/member-history/reservation")}
       />
-      <Swiper
-        spaceBetween={10}
-        slidesPerView={1}
-        style={{ overflow: "visible" }}
-        className="mt-2"
-      >
-        {upcomingReservations.map((reservation) => (
-          <SwiperSlide key={reservation.id}>
-            <ReserveCard reservation={reservation} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      {!upcomingReservations || upcomingReservations.length === 0 ? (
+        <EmptyCard
+          title={`예정된 예약이 없어요.\n예약을 통해 관리를 받아보세요.`}
+          button="예약하러 가기"
+          onClick={handleReservationClick}
+        />
+      ) : (
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={1}
+          style={{ overflow: "visible" }}
+          className="mt-2"
+        >
+          {upcomingReservations.map((reservation) => (
+            <SwiperSlide key={reservation.id}>
+              <ReserveCard reservation={reservation} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
     </div>
   )
 }
