61c744a4400734d9da593e26d77e1364a3bfe0ce
thumper-lee <thumper@onboarders.co.kr>
2025-02-07 16:34:42 +0900

    desgin: ButtonRound padding

--- src/components/ButtonRound.tsx
+++ src/components/ButtonRound.tsx
@@ -10,9 +10,9 @@ export const ButtonRound = (props: ButtonRoundProps) => {
     <>
       <button
         onClick={onClick}
-        className="h-9 px-4 bg-primary rounded-full font-sb text-12px text-white"
+        className="h-9 flex flex-col justify-center items-center px-4 bg-primary rounded-full font-sb text-12px text-white"
       >
-        {title}
+        <span className="whitespace-nowrap leading-[12px]">{title}</span>
       </button>
     </>
   )
