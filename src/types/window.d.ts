b5a1036287fa9c4842c6d6ae3d40be4cfaa9c22a
Rooge <rooge@onboarders.co.kr>
2025-02-08 15:43:20 +0900

    refactor: Update ReactNativeWebView type definition syntax

--- src/types/window.d.ts
+++ src/types/window.d.ts
@@ -1,7 +1,7 @@
 interface Window {
   ReactNativeWebView?: {
-    postMessage: (message: string): void
-    onMessage(value: string): void
+    postMessage: (message: string) => void
+    onMessage: (value: string) => void
   }
   webkit?: {
     messageHandlers: {
