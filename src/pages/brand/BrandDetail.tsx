ff2dae32c4610fc90a912b7bf5e81f0cc6400447
xoals25 <xoals25@naver.com>
2025-02-15 12:24:56 +0900

    fix: prevent error when imageUrl is empty in brandDetail

--- src/pages/brand/BrandDetail.tsx
+++ src/pages/brand/BrandDetail.tsx
@@ -23,9 +23,9 @@ export const BrandDetailPage = () => {
 
   return (
     <div className="relative w-full">
-      {/*TODO brand detail api 아사나 요청사항 확인후 재 변경 필요, /assets/brand_example.png 제거 */}
+      {/*TODO brand detail api 아사나 요청사항 확인후 재 변경 필요, /assets/brand_example.png 제거 후 imageUrl이 아예 존재 하지 않은 경우 처리 필요 */}
       <img
-        src={brand  && brand.length > 0 ? brand?.[0].imageUrl : "/assets/brand_example.png"}
+        src={brand && brand.length && brand[0].imageUrl ? brand[0].imageUrl : "/assets/brand_example.png"}
         alt=""
         className="w-full h-full"
       />
