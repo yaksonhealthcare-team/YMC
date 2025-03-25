4840f79a688f4b7bf48070430e101a93b0c92371
Jinha <58817064+jinha-ryu@users.noreply.github.com>
2025-03-25 21:31:47 +0900

    [THER-17] fix: 개인정보 처리방침 페이지 초기 스크롤 위치 수정

--- src/pages/terms/PrivacyTermsPage.tsx
+++ src/pages/terms/PrivacyTermsPage.tsx
@@ -1,9 +1,10 @@
 import { useLayout } from "../../contexts/LayoutContext.tsx"
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import { sampleTerms } from "./ServiceTermsPage.tsx"
 
 const PrivacyTermsPage = () => {
   const { setHeader, setNavigation } = useLayout()
+  const contentRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     setHeader({
@@ -14,8 +15,15 @@ const PrivacyTermsPage = () => {
     setNavigation({ display: false })
   }, [])
 
+  useEffect(() => {
+    // 컴포넌트가 마운트된 직후 실행
+    if (contentRef.current) {
+      contentRef.current.scrollIntoView({ behavior: "auto", block: "start" })
+    }
+  }, [])
+
   return (
-    <div className={"px-5 py-4"}>
+    <div ref={contentRef} className={"px-5 py-4"}>
       <p className={"font-b text-24px"}>{"개인정보 수집 이용"}</p>
       <p className={"whitespace-pre-wrap text-gray-600"}>{sampleTerms}</p>
     </div>
