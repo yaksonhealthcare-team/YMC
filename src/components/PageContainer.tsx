f663ad3bf4b77e808801b1a006ee7cb0bcdbfc02
Jinha <58817064+jinha-ryu@users.noreply.github.com>
2025-03-25 23:42:35 +0900

    fix: THER-35 모든 페이지 스크롤 새로고침 개선 - PageContainer 컴포넌트에 overscroll-none 클래스 추가

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from "react"
 
 const PageContainer = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="bg-gray-50 h-screen max-h-full overflow-x-hidden">
-      <div className="max-w-[500px] mx-auto bg-white h-full max-h-full overflow-y-scroll overflow-x-hidden flex flex-col scrollbar-hide">
+    <div className="bg-gray-50 h-screen max-h-full overflow-x-hidden overscroll-none">
+      <div className="max-w-[500px] mx-auto bg-white h-full max-h-full overflow-y-scroll overflow-x-hidden flex flex-col scrollbar-hide overscroll-none">
         {children}
       </div>
     </div>
