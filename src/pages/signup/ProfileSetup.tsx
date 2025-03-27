a350596c701707a5d6c60ab9ab540533918163d8
Jinha <58817064+jinha-ryu@users.noreply.github.com>
2025-03-27 14:45:36 +0900

    fix: THER-66 사용하지 않는 변수 제거

--- src/pages/signup/ProfileSetup.tsx
+++ src/pages/signup/ProfileSetup.tsx
@@ -21,8 +21,8 @@ export const ProfileSetup = () => {
   const isSocialSignup = !!sessionStorage.getItem("socialSignupInfo")
 
   const {
-    handleImageUpload,
-    handleImageDelete,
+    // handleImageUpload,
+    // handleImageDelete,
     handleCompletePostcode,
     toggleBrandSelection,
     handleNameChange,
