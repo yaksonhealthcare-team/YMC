b13d4a470f5ab061759386f2e1c8f7216c3b58ea
thumper-lee <thumper@onboarders.co.kr>
2025-03-28 11:33:10 +0900

    fix: floating button transition

--- src/pages/branch/_fragments/BranchMapSection.tsx
+++ src/pages/branch/_fragments/BranchMapSection.tsx
@@ -104,6 +104,7 @@ const BranchMapSection = ({
           onMoveMap: fetchBranchesByCoords,
           showCurrentLocationButton: true,
           showCurrentLocation: true,
+          currentLocationButtonPosition: selectedBranch ? "transition-transform -translate-y-32 duration-300" : "transition-transform translate-y-0 duration-300",
         }}
       />
       <div
