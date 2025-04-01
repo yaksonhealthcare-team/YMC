9fbb8db638c023f47f24fe4d126286a2af3d4e22
Jinha <58817064+jinha-ryu@users.noreply.github.com>
2025-04-01 22:15:32 +0900

    style: Membership 페이지 레이아웃 수정
    - 회원권 목록 영역에 배경색 추가
    - 중앙 정렬을 위한 mx-auto 클래스 추가
    - 로딩 인디케이터의 배경색 제거

--- src/pages/membership/Membership.tsx
+++ src/pages/membership/Membership.tsx
@@ -90,8 +90,8 @@ const MembershipPage = () => {
   }
 
   return (
-    <div className="min-h-screen bg-system-bg flex justify-center">
-      <div className="w-full max-w-[500px] relative">
+    <div className="min-h-screen bg-system-bg">
+      <div className="w-full max-w-[500px] mx-auto relative">
         {/* 고정 영역 */}
         <div className="fixed top-[48px] left-1/2 -translate-x-1/2 w-full max-w-[500px] z-10 bg-system-bg">
           {/* 안내 메시지 */}
@@ -199,11 +199,11 @@ const MembershipPage = () => {
         </div>
 
         {/* 회원권 목록 */}
-        <div className="pt-[145px] pb-[96px]">
+        <div className="pt-[145px] pb-[96px] bg-system-bg">
           <div className="max-w-[500px] min-w-[375px] mx-auto">
             <div className="px-5 py-6">
               {membershipsData.pages[0].body.length === 0 ? (
-                <div className="flex justify-center items-center h-[200px] text-gray-600 bg-system-bg">
+                <div className="flex justify-center items-center h-[200px] text-gray-600">
                   준비중 입니다
                 </div>
               ) : (
@@ -223,7 +223,7 @@ const MembershipPage = () => {
                   )}
                   <div ref={observerTarget} className="h-4" />
                   {isFetchingNextPage && (
-                    <LoadingIndicator className="min-h-[100px] bg-system-bg" />
+                    <LoadingIndicator className="min-h-[100px]" />
                   )}
                 </div>
               )}
