1558e7047dec4c3499f9621fe73f3fc3e0c8b8a7
Rooge <rooge@onboarders.co.kr>
2025-04-13 23:25:03 +0900

    fix: NoticeDetailPage에서 파일 목록 렌더링 방식 수정

--- src/pages/notice/NoticeDetail.tsx
+++ src/pages/notice/NoticeDetail.tsx
@@ -18,26 +18,30 @@ const NoticeDetailPage: React.FC = () => {
   // 페이지 마운트 시 상위 스크롤 컨테이너 초기화
   useEffect(() => {
     // 스크롤 컨테이너 찾기
-    const scrollContainer = document.querySelector('.max-w-\\[500px\\].mx-auto.bg-white.h-full.max-h-full.overflow-y-scroll');
+    const scrollContainer = document.querySelector(
+      ".max-w-\\[500px\\].mx-auto.bg-white.h-full.max-h-full.overflow-y-scroll",
+    )
     if (scrollContainer) {
       // 타이밍 이슈 방지를 위해 약간 지연시켜 실행
       setTimeout(() => {
-        (scrollContainer as HTMLElement).scrollTop = 0;
-      }, 0);
+        ;(scrollContainer as HTMLElement).scrollTop = 0
+      }, 0)
     }
-  }, []);
+  }, [])
 
   // 뒤로가기 클릭 핸들러 정의
   const handleBack = () => {
     // 먼저 상위 스크롤 컨테이너 초기화
-    const scrollContainer = document.querySelector('.max-w-\\[500px\\].mx-auto.bg-white.h-full.max-h-full.overflow-y-scroll');
+    const scrollContainer = document.querySelector(
+      ".max-w-\\[500px\\].mx-auto.bg-white.h-full.max-h-full.overflow-y-scroll",
+    )
     if (scrollContainer) {
-      (scrollContainer as HTMLElement).scrollTop = 0;
+      ;(scrollContainer as HTMLElement).scrollTop = 0
     }
-    
+
     // 이후 페이지 이동
-    navigate("/notice", { state: { from: fromPath } });
-  };
+    navigate("/notice", { state: { from: fromPath } })
+  }
 
   useEffect(() => {
     setHeader({
@@ -96,15 +100,21 @@ const NoticeContent: React.FC<{ notice: Notice }> = ({ notice }) => {
       <div className="text-16px font-normal text-gray-900 leading-[26.88px] whitespace-pre-wrap">
         {notice.contents}
       </div>
-      {notice.files?.length > 0 && notice.files[0].fileurl && (
-        <div className="mt-4 mb-[24px]">
-          <Image
-            src={notice.files[0].fileurl}
-            alt="공지사항 이미지"
-            className="w-full rounded-lg"
-          />
-        </div>
-      )}
+      {notice.files?.length > 0 &&
+        notice.files.map((file, index) => (
+          <div
+            className={`mt-4 ${
+              index === notice.files.length - 1 ? "mb-[24px]" : "mb-0"
+            }`}
+            key={file.fileCode}
+          >
+            <Image
+              src={file.fileurl}
+              alt="공지사항 이미지"
+              className="w-full rounded-lg"
+            />
+          </div>
+        ))}
     </div>
   )
 }
