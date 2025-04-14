77c5af19c6241c683096931136f3d3c1dccfb569
Jinha <58817064+jinha-ryu@users.noreply.github.com>
2025-04-15 01:07:31 +0900

    build: vite-plugin-compression 추가 및 Vite 설정에 압축 플러그인 적용

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,32 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import tsconfigPaths from "vite-tsconfig-paths"
 import svgr from "vite-plugin-svgr"
+import compression from "vite-plugin-compression"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: true,
   },
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgr(),
+    compression({
+      algorithm: "gzip", // 'gzip' | 'brotliCompress' | 'deflate' | 'deflateRaw'
+      ext: ".gz", // 압축된 파일의 확장자
+      threshold: 10240, // 압축이 시작되는 최소 파일 크기 (bytes)
+      deleteOriginFile: false, // 원본 파일 삭제 여부
+      verbose: true, // 로그 출력 여부
+    }),
+    compression({
+      algorithm: "brotliCompress",
+      ext: ".br",
+      threshold: 10240, // 10KB
+      deleteOriginFile: false,
+      verbose: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@assets": "/src/assets",
