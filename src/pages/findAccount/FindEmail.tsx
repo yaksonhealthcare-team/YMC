dc30fb5588da6e49507d53333cdce004b5d9a670
Rooge <rooge@onboarders.co.kr>
2025-04-19 21:42:49 +0900

    feat: FindEmail 컴포넌트에서 이메일 미제공 시 계정 찾기 메시지 및 버튼 추가
    이메일이 없는 경우 계정을 찾을 수 없다는 메시지와 로그인 페이지로 이동하는 버튼을 추가하여 사용자 경험을 개선함. 기존 로직을 수정하여 조건부 렌더링을 통해 이메일 유무에 따라 다른 메시지를 표시하도록 변경함.

--- src/pages/findAccount/FindEmail.tsx
+++ src/pages/findAccount/FindEmail.tsx
@@ -57,22 +57,54 @@ const FindEmail = () => {
     )
   }
 
-  return (
-    <div className="px-[20px] mt-[28px]">
-      <p className="flex flex-col justify-center items-center font-[600] text-20px text-[#212121]">
-        {loginInfo.thirdPartyType === "E"
-          ? "가입하신 이메일 계정입니다."
-          : `${loginInfo.thirdPartyType} 계정으로 가입되어 있습니다.`}
-      </p>
+  if (loginInfo.thirdPartyType === "E") {
+    if (!loginInfo.email) {
+      return (
+        <div className="px-[20px] mt-[28px]">
+          <p className="flex flex-col justify-center items-center font-[600] text-20px text-[#212121]">
+            계정을 찾을 수 없습니다.
+          </p>
+          <Button
+            variantType="primary"
+            sizeType="l"
+            className="h-[52px] mt-[40px] font-[700] text-16px w-full"
+            onClick={navigateToLogin}
+          >
+            로그인 페이지로 이동
+          </Button>
+        </div>
+      )
+    }
 
-      {loginInfo.thirdPartyType === "E" && loginInfo.email && (
+    return (
+      <div className="px-[20px] mt-[28px]">
+        <p className="flex flex-col justify-center items-center font-[600] text-20px text-[#212121]">
+          가입하신 이메일 계정입니다.
+        </p>
+
         <div className="flex justify-center items-center mt-[40px] h-[80px] w-full bg-[#FEF1F0] rounded-xl">
           <span className="font-[500]m text-16px text-primary-300">
             {loginInfo.email}
           </span>
         </div>
-      )}
 
+        <Button
+          variantType="primary"
+          sizeType="l"
+          className="h-[52px] mt-[40px] font-[700] text-16px w-full"
+          onClick={navigateToLogin}
+        >
+          로그인 페이지로 이동
+        </Button>
+      </div>
+    )
+  }
+
+  return (
+    <div className="px-[20px] mt-[28px]">
+      <p className="flex flex-col justify-center items-center font-[600] text-20px text-[#212121]">
+        {`${loginInfo.thirdPartyType} 계정으로 가입되어 있습니다.`}
+      </p>
       <Button
         variantType="primary"
         sizeType="l"
