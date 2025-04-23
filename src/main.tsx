83036645161932fe0e08381569a81f6b9fa2bcf6
Rooge <rooge@onboarders.co.kr>
2025-04-23 16:37:27 +0900

    chore: StrictMode 관련 주석 제거 및 App 컴포넌트 직접 렌더링으로 코드 간소화

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
 import "./styles/fonts.css"
 
-// 개발 환경에서만 StrictMode 사용
 const AppWithStrictMode = <App />
 
 ReactDOM.createRoot(document.getElementById("root")!).render(AppWithStrictMode)
