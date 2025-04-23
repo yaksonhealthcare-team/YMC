c6ce769ed759263d4b0fa0edcef67452d01196d3
Rooge <rooge@onboarders.co.kr>
2025-04-24 00:50:36 +0900

    design: MembershipRadioCard 레이아웃 개선

--- src/pages/reservation/_fragments/MembershipRadioCard.tsx
+++ src/pages/reservation/_fragments/MembershipRadioCard.tsx
@@ -1,5 +1,6 @@
-import { RadioCard } from "@components/RadioCard"
 import { MyMembership } from "../../../types/Membership"
+import { Radio } from "@mui/material"
+import clsx from "clsx"
 
 interface MembershipRadioCardProps {
   membership: MyMembership
@@ -30,31 +31,60 @@ export const MembershipRadioCard = ({
   }
 
   return (
-    <RadioCard checked={checked} value={value} className="items-start">
-      <div className="w-[206px] flex-col justify-start items-start gap-4">
-        <div className="self-stretch flex-col justify-start items-start gap-3">
-          <div className="px-[6px] py-[2px] bg-gray-100 rounded-[4px] justify-center items-center inline-flex mb-3">
-            <div className="text-center text-gray-500 text-12px font-m">
-              {displayType()}
+    <label
+      className={clsx(
+        "w-full p-5 bg-white rounded-xl border justify-between inline-flex cursor-pointer",
+        checked ? "border-primary" : "border-gray-100",
+      )}
+    >
+      <div className="flex-1">
+        <div className="flex-col justify-start items-start gap-4">
+          <div className="self-stretch flex-col justify-start items-start gap-3">
+            <div className="px-[6px] py-[2px] bg-gray-100 rounded-[4px] justify-center items-center inline-flex mb-3">
+              <div className="text-center text-gray-500 text-12px font-m">
+                {displayType()}
+              </div>
             </div>
-          </div>
-          <div className="self-stretch flex-col justify-start items-start gap-2">
-            <h3 className="text-gray-700 text-16px font-sb">
-              {membership.service_name || "회원권명"}
-            </h3>
+            <div className="self-stretch flex-col justify-start items-start gap-2">
+              <h3 className="text-gray-700 text-16px font-sb">
+                {membership.service_name || "회원권명"}
+              </h3>
 
-            <div className="flex items-center gap-[6px] mt-[8px]">
-              <span className="text-gray-600 text-12px">
-                {`${membership.remain_amount}회 / ${membership.buy_amount}회`}
-              </span>
-              <div className="w-[2px] h-[12px] bg-gray-200" />
-              <span className="text-gray-600 text-12px">
-                {`${formatDateWithDots(membership.pay_date)} - ${formatDateWithDots(membership.expiration_date)}`}
-              </span>
+              <div className="flex items-center gap-[6px] mt-[8px]">
+                <span className="text-gray-600 text-12px">
+                  {`${membership.remain_amount}회 / ${membership.buy_amount}회`}
+                </span>
+                <div className="w-[2px] h-[12px] bg-gray-200" />
+                <span className="text-gray-600 text-12px">
+                  {`${formatDateWithDots(membership.pay_date)} - ${formatDateWithDots(membership.expiration_date)}`}
+                </span>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </RadioCard>
+      <div className="relative h-full">
+        <Radio
+          checked={checked}
+          value={value}
+          sx={{
+            opacity: 0,
+            position: "absolute",
+            width: "20px",
+            height: "20px",
+          }}
+        />
+        <div className={clsx("w-5 h-5 relative")}>
+          {checked ? (
+            <>
+              <div className="w-5 h-5 left-0 top-0 absolute bg-primary rounded-full" />
+              <div className="w-2 h-2 left-[6px] top-[6px] absolute bg-white rounded-full" />
+            </>
+          ) : (
+            <div className="w-5 h-5 left-0 top-0 absolute rounded-full border-2 border-gray-200" />
+          )}
+        </div>
+      </div>
+    </label>
   )
 }
