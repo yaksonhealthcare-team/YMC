edaabc4f7259367b863f9077f4b4e44854f5b8dd
Jinha <58817064+jinha-ryu@users.noreply.github.com>
2025-04-24 17:10:05 +0900

    fix: 홈 카드 그림자 처리 및 디자인 개선

--- src/pages/home/_fragments/DynamicHomeHeaderBackground.tsx
+++ src/pages/home/_fragments/DynamicHomeHeaderBackground.tsx
@@ -2,7 +2,7 @@ import { memo, ReactNode, useEffect, useRef, useState } from "react"
 
 const ContentNode = memo(({ content }: { content: ReactNode }) => {
   return (
-    <div className="relative w-full bg-cover bg-center px-8">{content}</div>
+    <div className="relative w-full bg-cover bg-center px-5">{content}</div>
   )
 })
 
@@ -20,50 +20,55 @@ const DynamicHomeHeaderBackground = memo(
   }) => {
     return (
       <div
-        className="relative flex flex-col px-5"
+        className="relative flex flex-col px-5 py-2"
         style={{
-          width: "100%",
+          width: "calc(100%)",
+          filter: "drop-shadow(0px 1px 4px rgba(46, 43, 41, 0.12))",
         }}
       >
-        <div className="relative w-full">
-          <img
-            src="/assets/home_card_top.png"
-            alt=""
-            className="absolute top-0 left-0 w-full h-full"
+        {/* 상단 SVG - 가로 사이즈에 맞추고 비율 유지 */}
+        <div className="relative w-full mt-1">
+          <svg
             width="100%"
-            height="100%"
-          />
-          <div className="relative w-full h-24">
+            viewBox="0 0 335 81"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="xMidYMid slice"
+          >
+            <path
+              d="M259 0C270.046 7.08729e-07 279 8.95431 279 20V24C279 41.6731 293.327 56 311 56H315C326.046 56 335 64.9543 335 76V81H0V20C6.76515e-07 8.95431 8.95431 4.22794e-07 20 0H259Z"
+              fill="white"
+            />
+          </svg>
+          <div className="absolute top-3 left-3 w-full h-24">
             <div className="flex justify-between w-full">
-              <div className={"pt-5 mt-5 ml-8 w-full max-w-[84%]"}>
-                {header}
-              </div>
+              <div className={"px-2 pt-5 w-full max-w-[84%]"}>{header}</div>
               <DynamicSquareContainer>{buttonArea}</DynamicSquareContainer>
             </div>
           </div>
         </div>
 
-        <div className="relative w-full pt-1">
-          <img
-            src="/assets/home_card_mid.png"
-            alt=""
-            className="absolute top-0 left-0 w-full h-full"
-            width="100%"
-            height="100%"
-          />
+        {/* 중간 부분 - 가변적인 높이, 위쪽 1px 겹침 */}
+        <div className="relative w-full bg-white" style={{ marginTop: "-1px" }}>
           {content && <ContentNode content={content} />}
-
           {contents &&
             contents.map((item, i) => <ContentNode key={i} content={item} />)}
         </div>
-        <div>
-          <img
-            src="/assets/home_card_bottom.png"
-            alt=""
-            className="w-full"
+
+        {/* 하단 SVG - 가로 사이즈에 맞추고 비율 유지, 위쪽 1px 겹침 */}
+        <div className="relative w-full mb-1" style={{ marginTop: "-1px" }}>
+          <svg
             width="100%"
-            height="20px"
-          />
+            viewBox="0 0 335 26"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="xMidYMid slice"
+          >
+            <path
+              d="M335 6C335 17.0457 326.046 26 315 26H20C8.95431 26 0 17.0457 0 6V0H335V6Z"
+              fill="white"
+            />
+          </svg>
         </div>
       </div>
     )
@@ -99,7 +104,7 @@ const DynamicSquareContainer = ({
   return (
     <div
       ref={containerRef}
-      className={`relative mr-3 mt-3 pl-3 pb-3 w-[14%] ${className}`}
+      className={`relative w-[14%] ${className}`}
       style={{ height: `${size}px` }}
     >
       <div className="absolute inset-0">{children}</div>
