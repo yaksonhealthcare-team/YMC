aac83d47f343c6bf224774df92b57f8666e38031
Rooge <rooge@onboarders.co.kr>
2025-04-30 18:38:20 +0900

    feat: Store 페이지에 로딩 인디케이터 추가

--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -1,10 +1,12 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useLayout } from "contexts/LayoutContext"
 import { axiosClient } from "queries/clients"
+import LoadingIndicator from "@components/LoadingIndicator"
 
 const Store = () => {
   const iframeRef = useRef<HTMLIFrameElement>(null)
   const { setHeader, setNavigation } = useLayout()
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     setHeader({ display: false, backgroundColor: "bg-white" })
@@ -22,6 +24,7 @@ const Store = () => {
           },
           "https://mall.yaksonhc.com/",
         )
+        setIsLoading(false)
       }
     }
 
@@ -42,6 +45,11 @@ const Store = () => {
       className="bg-white"
       style={{ width: "100%", height: "calc(100vh - 82px)" }}
     >
+      {isLoading && (
+        <div className="flex justify-center items-center h-full w-full absolute top-0 left-0 z-10 bg-white opacity-50">
+          <LoadingIndicator />
+        </div>
+      )}
       <iframe
         ref={iframeRef}
         src="https://mall.yaksonhc.com/"
