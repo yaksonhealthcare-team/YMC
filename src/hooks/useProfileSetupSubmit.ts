4e8e7527981b5df2d6a1a8dfc8ce86b4b97dd5f5
Rooge <rooge@onboarders.co.kr>
2025-05-06 15:59:52 +0900

    refactor: 불필요한 try/catch 제거

--- src/hooks/useProfileSetupSubmit.ts
+++ src/hooks/useProfileSetupSubmit.ts
@@ -36,140 +36,132 @@ export const useProfileSetupSubmit = () => {
   const { signupData, cleanup } = useSignup()
 
   const handleSocialSignup = async (socialInfo: SocialSignupInfo) => {
-    try {
-      const response = await signupWithSocial({
-        thirdPartyType: socialInfo.thirdPartyType,
-        userInfo: {
-          ...socialInfo,
-          name: signupData.name,
-          email: signupData.email,
-          mobileno: signupData.mobileNumber,
-          birthdate: signupData.birthDate,
-          gender: signupData.gender,
-          di: signupData.di,
-          token_version_id: signupData.tokenVersionId,
-          post: signupData.postCode,
-          addr1: signupData.address1,
-          addr2: signupData.address2 || "",
-          marketing_yn: socialInfo.SocialRefreshToken ? "Y" : "N",
-          brand_code: signupData.brandCodes || [],
-          profileUrl: signupData.profileUrl,
-          profileURL: signupData.profileUrl,
-        },
-      })
+    const response = await signupWithSocial({
+      thirdPartyType: socialInfo.thirdPartyType,
+      userInfo: {
+        ...socialInfo,
+        name: signupData.name,
+        email: signupData.email,
+        mobileno: signupData.mobileNumber,
+        birthdate: signupData.birthDate,
+        gender: signupData.gender,
+        di: signupData.di,
+        token_version_id: signupData.tokenVersionId,
+        post: signupData.postCode,
+        addr1: signupData.address1,
+        addr2: signupData.address2 || "",
+        marketing_yn: socialInfo.SocialRefreshToken ? "Y" : "N",
+        brand_code: signupData.brandCodes || [],
+        profileUrl: signupData.profileUrl,
+        profileURL: signupData.profileUrl,
+      },
+    })
 
-      if (!response?.body?.length || !response.body[0]?.accessToken) {
-        throw new Error(
-          response?.resultMessage ?? "회원가입 응답이 올바르지 않습니다",
-        )
-      }
-
-      const signinResponse = await signinWithSocial({
-        thirdPartyType: socialInfo.thirdPartyType,
-        SocialAccessToken: response.body[0].accessToken,
-        socialId: socialInfo.socialId,
-        id_token: socialInfo.id_token,
-        SocialRefreshToken: socialInfo.SocialRefreshToken,
-        deviceToken: socialInfo.deviceToken ?? (await requestForToken()),
-        deviceType: socialInfo.deviceType ?? window.osType ?? "web",
-      })
-
-      const accessToken = signinResponse.data.body[0].accessToken
-
-      if (!accessToken) {
-        throw new Error(
-          "회원가입에 실패했습니다. 계속 문제가 발생할 경우 고객센터에 문의해 주세요.",
-        )
-      }
-
-      setAccessToken(accessToken)
-
-      // ReactNativeWebView 환경에서 localStorage에 accessToken 저장
-      if (window.ReactNativeWebView) {
-        saveAccessToken(accessToken)
-
-        // ReactNativeWebView로 accessToken 전달
-        window.ReactNativeWebView.postMessage(
-          JSON.stringify({
-            type: "LOGIN_SUCCESS",
-            data: {
-              accessToken: accessToken,
-            },
-          }),
-        )
-      }
-
-      const user = await fetchUser()
-      login({ user })
-      cleanup()
-      navigate("/signup/complete", { replace: true })
-    } catch (error: unknown) {
-      throw error
+    if (!response?.body?.length || !response.body[0]?.accessToken) {
+      throw new Error(
+        response?.resultMessage ?? "회원가입 응답이 올바르지 않습니다",
+      )
     }
+
+    const signinResponse = await signinWithSocial({
+      thirdPartyType: socialInfo.thirdPartyType,
+      SocialAccessToken: response.body[0].accessToken,
+      socialId: socialInfo.socialId,
+      id_token: socialInfo.id_token,
+      SocialRefreshToken: socialInfo.SocialRefreshToken,
+      deviceToken: socialInfo.deviceToken ?? (await requestForToken()),
+      deviceType: socialInfo.deviceType ?? window.osType ?? "web",
+    })
+
+    const accessToken = signinResponse.data.body[0].accessToken
+
+    if (!accessToken) {
+      throw new Error(
+        "회원가입에 실패했습니다. 계속 문제가 발생할 경우 고객센터에 문의해 주세요.",
+      )
+    }
+
+    setAccessToken(accessToken)
+
+    // ReactNativeWebView 환경에서 localStorage에 accessToken 저장
+    if (window.ReactNativeWebView) {
+      saveAccessToken(accessToken)
+
+      // ReactNativeWebView로 accessToken 전달
+      window.ReactNativeWebView.postMessage(
+        JSON.stringify({
+          type: "LOGIN_SUCCESS",
+          data: {
+            accessToken: accessToken,
+          },
+        }),
+      )
+    }
+
+    const user = await fetchUser()
+    login({ user })
+    cleanup()
+    navigate("/signup/complete", { replace: true })
   }
 
   const handleEmailSignup = async () => {
-    try {
-      const signupFormData = {
-        userInfo: {
-          name: signupData.name,
-          email: signupData.email,
-          password: signupData.password,
-          mobileno: signupData.mobileNumber,
-          birthdate: signupData.birthDate,
-          gender: signupData.gender,
-          addr1: signupData.address1,
-          addr2: signupData.address2 || "",
-          marketing_yn: signupData.marketingYn,
-          post: signupData.postCode,
-          nationalinfo: "0",
-          brand_code: signupData.brandCodes || [],
-          profileUrl: signupData.profileUrl,
-          profileURL: signupData.profileUrl,
-        },
-        authData: {
-          di: signupData.di,
-          token_version_id: signupData.tokenVersionId,
-        },
-        optional: {
-          recom: signupData.referralCode,
-        },
-      }
-
-      await signup(signupFormData)
-
-      const loginResponse = await loginWithEmail({
-        username: signupData.email,
+    const signupFormData = {
+      userInfo: {
+        name: signupData.name,
+        email: signupData.email,
         password: signupData.password,
-        deviceToken: await requestForToken(),
-        deviceType: window.osType ?? "web",
-      })
-
-      const accessToken = loginResponse.accessToken
-
-      if (!accessToken) {
-        throw new Error("로그인에 실패했습니다.")
-      }
-
-      // ReactNativeWebView 환경에서 accessToken 전달
-      if (window.ReactNativeWebView) {
-        window.ReactNativeWebView.postMessage(
-          JSON.stringify({
-            type: "LOGIN_SUCCESS",
-            data: {
-              accessToken: accessToken,
-            },
-          }),
-        )
-      }
-
-      const user = await fetchUser()
-      login({ user })
-      cleanup()
-      navigate("/signup/complete", { replace: true })
-    } catch (error: unknown) {
-      throw error
+        mobileno: signupData.mobileNumber,
+        birthdate: signupData.birthDate,
+        gender: signupData.gender,
+        addr1: signupData.address1,
+        addr2: signupData.address2 || "",
+        marketing_yn: signupData.marketingYn,
+        post: signupData.postCode,
+        nationalinfo: "0",
+        brand_code: signupData.brandCodes || [],
+        profileUrl: signupData.profileUrl,
+        profileURL: signupData.profileUrl,
+      },
+      authData: {
+        di: signupData.di,
+        token_version_id: signupData.tokenVersionId,
+      },
+      optional: {
+        recom: signupData.referralCode,
+      },
     }
+
+    await signup(signupFormData)
+
+    const loginResponse = await loginWithEmail({
+      username: signupData.email,
+      password: signupData.password,
+      deviceToken: await requestForToken(),
+      deviceType: window.osType ?? "web",
+    })
+
+    const accessToken = loginResponse.accessToken
+
+    if (!accessToken) {
+      throw new Error("로그인에 실패했습니다.")
+    }
+
+    // ReactNativeWebView 환경에서 accessToken 전달
+    if (window.ReactNativeWebView) {
+      window.ReactNativeWebView.postMessage(
+        JSON.stringify({
+          type: "LOGIN_SUCCESS",
+          data: {
+            accessToken: accessToken,
+          },
+        }),
+      )
+    }
+
+    const user = await fetchUser()
+    login({ user })
+    cleanup()
+    navigate("/signup/complete", { replace: true })
   }
 
   const handleError = (error: unknown) => {
