a74e04c9866afbc59d6cc511eac3bbef7c5745f4
yong-min-lee <yongmin.lee@yaksonhealthcare.com>
2025-07-02 17:59:06 +0900

    FEAT: 멤버십 로직 추가
    - 멤버십 스와이퍼 컴포넌트 사용을 위한 데이터 변환 비즈니스 로직 추가
    - 유저의 멤버십 메뉴 서비스, 쿼리, 타입 추가

---
+++ src/_domain/membership/business/index.ts
@@ -0,0 +1,1 @@
+export { convertMembership } from './membership.business';
---
+++ src/_domain/membership/types/membership.types.ts
@@ -0,0 +1,81 @@
+export interface UserMembershipParams {
+  /**
+   * 검색 타입
+   * '': 전체
+   * T: 사용가능
+   * F: 사용완료
+   * E: 만료
+   */
+  searchType: '' | 'T' | 'F' | 'E';
+
+  /**
+   * 페이지 숫자
+   */
+  page?: number;
+}
+
+export interface UserMembershipSchema {
+  /**
+   * 회원권 일련번호
+   */
+  mp_idx: string;
+
+  /**
+   * 상태
+   */
+  status: string;
+
+  /**
+   * 회원권 이름
+   */
+  service_name: string;
+
+  s_type: string;
+
+  /**
+   * 지점 정보
+   */
+  branchs: Branch[];
+
+  /**
+   * 남은 금액
+   */
+  remain_amount: string;
+
+  /**
+   * 구매한 금액
+   */
+  buy_amount: string;
+
+  /**
+   * 결제일
+   */
+  pay_date: string;
+
+  /**
+   * 만료일
+   */
+  expiration_date: string;
+
+  /**
+   * 회원권 구분
+   * F: 정액권
+   * M: 횟수권
+   */
+  mp_gubun: 'F' | 'M';
+}
+
+/**
+ * 지점
+ */
+export interface Branch {
+  /**
+   * 지점 ID
+   */
+  b_idx: string;
+
+  /**
+   * 지점 이름
+   */
+  b_name: string;
+}
