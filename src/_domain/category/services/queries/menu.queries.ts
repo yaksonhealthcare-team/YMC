139af827c7e8511db6a099d3304a87167e383fd5
yong-min-lee <yongmin.lee@yaksonhealthcare.com>
2025-07-04 10:10:39 +0900

    FEAT: 정액권 관련 로직 추가
    - 서비스, 쿼리, 타입, 비즈니스 로직 추가

--- src/_domain/category/services/queries/menu.queries.ts
+++ src/_domain/category/services/queries/menu.queries.ts
@@ -2,8 +2,8 @@ import { ListResponse } from '@/_shared/types/response.types';
 import { CustomUseInfiniteQueryOptions } from '@/_shared/types/util.types';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { AxiosError, AxiosResponse } from 'axios';
-import { ConsultMenuParams, ConsultMenuSchema } from '../../types/menu.types';
-import { getConsultMenu } from '../menu.services';
+import { ConsultMenuParams, ConsultMenuSchema, PrepaidMenuParams, PrepaidMenuSchema } from '../../types/menu.types';
+import { getConsultMenu, getPrepaidMenu } from '../menu.services';
 
 export const useGetConsultMenu = (
   params: ConsultMenuParams,
@@ -20,6 +20,30 @@ export const useGetConsultMenu = (
   >({
     queryKey: ['get-consultation-memberships', params],
     queryFn: ({ pageParam = 1 }) => getConsultMenu({ ...params, page: pageParam as number }),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => {
+      return lastPage.data.current_page < lastPage.data.total_page_count ? lastPage.data.current_page + 1 : undefined;
+    },
+    select: (data) => data.pages.flatMap((page) => page),
+    ...options
+  });
+};
+
+export const useGetPrepaidMenu = (
+  params: PrepaidMenuParams,
+  options?: CustomUseInfiniteQueryOptions<
+    AxiosResponse<ListResponse<PrepaidMenuSchema>>,
+    AxiosError,
+    AxiosResponse<ListResponse<PrepaidMenuSchema>>[]
+  >
+) => {
+  return useInfiniteQuery<
+    AxiosResponse<ListResponse<PrepaidMenuSchema>>,
+    AxiosError,
+    AxiosResponse<ListResponse<PrepaidMenuSchema>>[]
+  >({
+    queryKey: ['get-flat-memberships', params],
+    queryFn: ({ pageParam = 1 }) => getPrepaidMenu({ ...params, page: pageParam as number }),
     initialPageParam: 1,
     getNextPageParam: (lastPage) => {
       return lastPage.data.current_page < lastPage.data.total_page_count ? lastPage.data.current_page + 1 : undefined;
