63844f7b7cd0a383009aeaf77f25e85c1842f879
yong-min-lee <yongmin.lee@yaksonhealthcare.com>
2025-08-13 14:45:44 +0900

    REFACTOR: 불필요한 코드 제거

--- src/libs/apple.ts
+++ src/libs/apple.ts
@@ -3,7 +3,6 @@ const APPLE_REDIRECT_URI = import.meta.env.VITE_APPLE_REDIRECT_URI;
 
 export const getAppleLoginUrl = () => {
   const state = Math.random().toString(36).substr(2, 11);
-  localStorage.setItem('appleState', state);
 
   return `https://appleid.apple.com/auth/authorize?client_id=${APPLE_CLIENT_ID}&redirect_uri=${APPLE_REDIRECT_URI}&response_type=code%20id_token&scope=name%20email&response_mode=form_post&state=${state}`;
 };
--- src/libs/google.ts
+++ src/libs/google.ts
@@ -3,7 +3,6 @@ const GOOGLE_REDIRECT_URI = import.meta.env.VITE_GOOGLE_REDIRECT_URI;
 
 export const getGoogleLoginUrl = async () => {
   const state = Math.random().toString(36).substr(2, 11);
-  localStorage.setItem('googleState', state);
 
   return `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URI}&scope=email%20profile&state=${state}`;
 };
