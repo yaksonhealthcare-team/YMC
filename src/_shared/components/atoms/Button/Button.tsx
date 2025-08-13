1fce660a58ba3125f08280738732ecd184132e2f
yong-min-lee <yongmin.lee@yaksonhealthcare.com>
2025-08-13 11:14:58 +0900

    FEAT: 컴포넌트 업데이트

--- src/_shared/components/atoms/Button/Button.tsx
+++ src/_shared/components/atoms/Button/Button.tsx
@@ -7,16 +7,19 @@ export const Button = ({
   variant = 'primary',
   size = 'large',
   isLoading = false,
-  className,
+  className = '',
   ...props
 }: ButtonProps) => {
+  const hasCustomBg = /\bbg-/.test(className);
+  const style = getStyles(variant, size, hasCustomBg);
+
   return (
     <button
       disabled={isLoading}
       className={clsx(
         'normal-case w-full rounded-lg transition-colors duration-200 ease-in-out disabled:cursor-not-allowed',
         isLoading && 'flex justify-center',
-        getStyles(variant, size),
+        style,
         className
       )}
       {...props}
@@ -26,12 +29,29 @@ export const Button = ({
   );
 };
 
-const getStyles = (variant: ButtonVariant, size: ButtonSize) => {
-  return clsx(VARIANT_STYLES[variant], SIZE_STYLES[size]);
+const getStyles = (variant: ButtonVariant, size: ButtonSize, hasCustomBg: boolean) => {
+  const variantStyles = hasCustomBg
+    ? removeBgColor(VARIANT_STYLES[variant]) // bg-를 다 지움
+    : VARIANT_STYLES[variant];
+
+  return clsx(variantStyles, SIZE_STYLES[size]);
+};
+
+/**
+ * @deprecated
+ * tailwind.config.js 에 important: true로 되어있어
+ * 외부에서 className 주입으로 backgroundColor 오버라이딩이 안되기 때문에 사용
+ * 해결되면 제거 필요함
+ */
+const removeBgColor = (classes: string) => {
+  return classes
+    .split(' ')
+    .filter((c) => !c.startsWith('bg-'))
+    .join(' ');
 };
 
 const VARIANT_STYLES: Record<ButtonVariant, string> = {
-  primary: 'bg-primary text-white active:bg-primary-500 disabled:bg-[#DCDCDC] disabled:text-gray-400',
+  primary: 'bg-primary text-white active:opacity-70 disabled:bg-[#DCDCDC] disabled:text-gray-400',
   secondary: 'bg-secondary text-primary-400 active:bg-primary-200 disabled:bg-grey-50 disabled:text-gray-300'
 };
 
--- src/_shared/components/atoms/Loading/Loading.types.ts
+++ src/_shared/components/atoms/Loading/Loading.types.ts
@@ -4,4 +4,9 @@ export interface LoadingProps {
   size?: number;
 }
 
-export type LoadingVariant = 'primary' | 'button';
+/**
+ * - primary: 기본적으로 쓰이는 스타일
+ * - button: 버튼에 쓰이는 스타일
+ * - global: 전체 뷰포트를 기준으로 중앙에 위치하는 스타일
+ */
+export type LoadingVariant = 'primary' | 'button' | 'global';
