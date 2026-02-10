3df46b1b1d489fe76d80be6d7e92f0e0cb957377
yong-min-lee <yongmin.lee@yaksonhealthcare.com>
2026-02-10 13:36:06 +0900

    CHORE: jest 추가 및 세팅

---
+++ jest.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.test.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: '<rootDir>/tsconfig.app.json'
+      }
+    ]
+  }
+};
