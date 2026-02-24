df0257a13fe9e8472eec47b991df79983fd74f06
yong-min-lee <yongmin.lee@yaksonhealthcare.com>
2026-02-24 13:43:39 +0900

    CHORE: speed insights 업데이트

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import ReactDOM from 'react-dom/client';
 import 'swiper/swiper-bundle.css';
 import App from './App';
@@ -6,4 +7,9 @@ import './styles/global.css';
 import './styles/index.css';
 import './styles/swiper-custom.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <>
+    <App />
+    <SpeedInsights />
+  </>,
+);
